71287f0a772744580a2980c161de17605af5a4ee
Werner Strydom <werners@bloudraak.com>
2016-01-16 13:52:19 -0800

    Fixing Signature Check

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-wget https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.zip
-wget https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.zip.sign
+SYSLINUXVER="6.03"
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 58F7ABFE
-gpg --verify syslinux-6.03.zip.sign 
+mkdir -p tmp
+pushd tmp
 
+wget "https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-$SYSLINUXVER.zip" -O "syslinux-$SYSLINUXVER.zip"
+wget "https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-$SYSLINUXVER.zip.sign" -O "syslinux-$SYSLINUXVER.zip.sign"
+
+gpg --recv-keys 58F7ABFE
+gpg --verify "syslinux-$SYSLINUXVER.zip.sign" 
+
+popd 
